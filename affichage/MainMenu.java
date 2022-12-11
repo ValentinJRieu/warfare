e04a9cc81ec583c3b37f4cb6b1f4aad31e9edc5e
nico78916 <nico789.rf@gmail.com>
2022-12-11 23:50:27 +0100

    next commit : Les textures !

--- affichage/MainMenu.java
+++ affichage/MainMenu.java
@@ -1,7 +1,13 @@
 package wargame.affichage;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+import java.io.File;
+import java.io.IOException;
 
 public class MainMenu extends JPanel {
     MainMenu(JFrame frame){
@@ -19,5 +25,21 @@ public class MainMenu extends JPanel {
         this.add(loadButton);
         this.add(settingsButton);
         frame.pack();
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        try {
+            g.setClip(CaseTable.createHexagon(new Point(200,200),50));
+            BufferedImage ig = ImageIO.read(new File("C:\\Users\\NicoAngel\\IdeaProjects\\warfare\\textures\\terrain\\grass_1.png"));
+            TexturePaint tp = new TexturePaint(ig,new Rectangle(0,0,500, 500));
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setPaint(tp);
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.fill(CaseTable.createHexagon(new Point(300,300),500));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
