1b694e72e971a469e5a4023f798a31ba311de68d
nico78916 <nico789.rf@gmail.com>
2023-01-10 15:55:15 +0100

    ajout du boutton inverseFinTour et bind de la touche T à inverseFinTour

--- affichage/GameDownBar.java
+++ affichage/GameDownBar.java
@@ -2,6 +2,8 @@ package wargame.affichage;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class GameDownBar extends JPanel {
     public GameDownBar(GameWindow parent){
@@ -9,5 +11,19 @@ public class GameDownBar extends JPanel {
         Dimension dim = new Dimension(parent.getWidth() * 9/10, parent.getHeight() * 1/10);
         this.setPreferredSize(dim);
         this.setMaximumSize(dim);
+        JButton b1 = new JButton("Fin tour (t)");
+        add(b1);
+        b1.addMouseListener(new MouseAdapter() {
+            /**
+             * {@inheritDoc}
+             *
+             * @param e
+             */
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                parent.gameDisplay.getCarte().inverseFinTour();
+            }
+        });
     }
 }
